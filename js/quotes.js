edb1e002f6d2db7698a2fdbe1d7b8d9baf19b92a
Blake Burroughs <b.burro@gmail.com>
2019-02-15 17:47:16 -0600

    Everything Works added quotes to external file

--- js/quotes.js
+++ js/quotes.js
@@ -1,74 +1,62 @@
 let quotes = [
   {
-    quote: "Here is a quote.",
-    source: "Autobiography",
-    citation: "Book",
-    year: 1977,
-    tags: Historical
+    quote: "Where there is shouting, there is no true knowledge.",
+    source: "Leonardo da Vinci",
+    tags: 'wisdom'
   },
   {
-    quote: "Here is a quote.",
-    source: "Autobiography",
-    citation: "Book",
-    year: 1977,
-    tags: Historical
+    quote: "Be not wise in thine own eyes; fear the LORD, and depart from evil.",
+    source: "unknown",
+    citation: "Proverbs 3:7",
+    year: '700 bc',
+    tags: 'biblical'
   },
   {
-    quote: "Here is a quote.",
-    source: "Autobiography",
-    citation: "Book",
-    year: 1977,
-    tags: Historical
+    quote: "Dear children, let us not love with words or speech but with actions and in truth.",
+    source: "John the Apostle",
+    citation: "1 John 3:18",
+    year: 'circa AD 100',
+    tags: 'biblical'
   },
   {
-    quote: "Here is a quote.",
-    source: "Autobiography",
-    citation: "Book",
-    year: 1977,
-    tags: Historical
+    quote: "It does not matter how slowly you go as long as you do not stop.",
+    source: "Confucius",
+    year: 'circa 528 bc',
+    tags: 'wisdom'
   },
   {
-    quote: "Here is a quote.",
-    source: "Autobiography",
-    citation: "Book",
-    year: 1977,
-    tags: Historical
+    quote: "Kindness is the language which the deaf can hear and the blind can see.",
+    source: "Mark Twain",
+    year: 'circa 1890',
+    tags: 'author'
   },
   {
-    quote: "Here is a quote.",
-    source: "Autobiography",
-    citation: "Book",
-    year: 1977,
-    tags: Historical
+    quote: "What good is the warmth of summer, without the cold of winter to give it sweetness.",
+    source: "John Steinbeck",
+    year: 'circa 1950',
+    tags: 'author'
   },
   {
-    quote: "Here is a quote.",
-    source: "Autobiography",
-    citation: "Book",
-    year: 1977,
-    tags: Historical
+    quote: "Goals on the road to achievement cannot be achieved without discipline and consistency.",
+    source: "Denzel Washington",
+    year: 'circa 1996',
+    tags: 'actor'
   },
   {
-    quote: "Here is a quote.",
-    source: "Autobiography",
-    citation: "Book",
-    year: 1977,
-    tags: Historical
+    quote: "Optimism is the faith that leads to achievement.",
+    source: "Helen Keller",
+    tags: 'wise women'
   },
   {
-    quote: "Here is a quote.",
-    source: "Autobiography",
-    citation: "Book",
-    year: 1977,
-    tags: Historical
+    quote: "Knowing what must be done does away with fear.",
+    source: "Rosa Parks",
+    year: 'circa 1980',
+    tags: 'wise women'
   },
   {
-    quote: "Here is a quote.",
-    source: "Autobiography",
-    citation: "Book",
-    year: 1977,
-    tags: Historical
+    quote: "It's one of the greatest gifts you can give yourself, to forgive. Forgive everybody.",
+    source: "Maya Angelou",
+    year: 'circa 1995',
+    tags: 'wise women'
   },
 ];
-
-export { quotes };
--- js/script.js
+++ js/script.js
@@ -10,70 +10,6 @@ FSJS project 1 - A Random Quote Generator
 Create array of quotes.
 Would like to import quotes from external file but my syntax fell short.
 ***/
-let quotes = [
-  {
-    quote: "Where there is shouting, there is no true knowledge.",
-    source: "Leonardo da Vinci",
-    tags: 'wisdom'
-  },
-  {
-    quote: "Be not wise in thine own eyes; fear the LORD, and depart from evil.",
-    source: "unknown",
-    citation: "Proverbs 3:7",
-    year: '700 bc',
-    tags: 'biblical'
-  },
-  {
-    quote: "Dear children, let us not love with words or speech but with actions and in truth.",
-    source: "John the Apostle",
-    citation: "1 John 3:18",
-    year: 'circa AD 100',
-    tags: 'biblical'
-  },
-  {
-    quote: "It does not matter how slowly you go as long as you do not stop.",
-    source: "Confucius",
-    year: 'circa 528 bc',
-    tags: 'wisdom'
-  },
-  {
-    quote: "Kindness is the language which the deaf can hear and the blind can see.",
-    source: "Mark Twain",
-    year: 'circa 1890',
-    tags: 'author'
-  },
-  {
-    quote: "What good is the warmth of summer, without the cold of winter to give it sweetness.",
-    source: "John Steinbeck",
-    year: 'circa 1950',
-    tags: 'author'
-  },
-  {
-    quote: "Goals on the road to achievement cannot be achieved without discipline and consistency.",
-    source: "Denzel Washington",
-    year: 'circa 1996',
-    tags: 'actor'
-  },
-  {
-    quote: "Optimism is the faith that leads to achievement.",
-    source: "Helen Keller",
-    tags: 'wise women'
-  },
-  {
-    quote: "Knowing what must be done does away with fear.",
-    source: "Rosa Parks",
-    year: 'circa 1980',
-    tags: 'wise women'
-  },
-  {
-    quote: "It's one of the greatest gifts you can give yourself, to forgive. Forgive everybody.",
-    source: "Maya Angelou",
-    year: 'circa 1995',
-    tags: 'wise women'
-  },
-];
-
-
 
 /***
   FX getRandomQuote takes an array of wuotes and returns random quote by generating
@@ -130,7 +66,7 @@ $(".tags").hide().fadeIn(5000);
 
 };
 
-// FXs for changing background getColor
+// FXs for changing colors
 
 
 //use FX getColor and set background color to value while also looking at
